1c69231873a913058f19464487a6e2a539457002
G-J van Rooyen <g-j@octoco.ltd>
2022-12-01 13:39:27 +0200

    Solved Day 1, Part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,18 +2,24 @@
 // Read batches of numbers from the input file, and find the batch with the largest sum.
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Seek};
 
-fn argmax_calories<R: BufRead>(reader: &mut R) -> i32 {
-    let mut max = 0;
+fn argmax_calories<R: BufRead>(reader: &mut R, top_n: usize) -> i32 {
+    let mut max = vec![0; top_n];
+    let mut min_idx = 0;
+    let mut min = 0;
     let mut sum = 0;
 
     reader.lines().for_each(|line| {
         // If the line is empty, we've reached the end of a batch. Check whether the
         // sum is larger than the previous maximum, save it if it is, and reset the sum.
         if line.as_ref().unwrap().is_empty() {
-            if sum > max {
-                max = sum;
+            if sum > min {
+                println!("Had: {:?}, adding {}", max, sum);
+                max[min_idx] = sum;
+                min_idx = max.iter().enumerate().min_by_key(|(_, &v)| v).unwrap().0;
+                min = max[min_idx];
+                println!("Now: {:?}", max);
             }
             sum = 0;
         } else {
@@ -23,16 +29,21 @@ fn argmax_calories<R: BufRead>(reader: &mut R) -> i32 {
     });
 
     // If we've reached to end of the file, we still need to check the last batch.
-    if sum > max {
-        max = sum;
+    if sum > min {
+        max[min_idx] = sum;
     }
 
-    max
+    println!("Final: {:?}", max);
+    max.iter().sum()
 }
 
 fn main() {
     let file = File::open("data/input.txt").unwrap();
     let mut reader = BufReader::new(file);
-    let result = argmax_calories(&mut reader);
+    let result = argmax_calories(&mut reader, 1);
     println!("Part 1: {:?}", result);
+
+    reader.rewind().unwrap();
+    let result = argmax_calories(&mut reader, 3);
+    println!("Part 2: {:?}", result);
 }
