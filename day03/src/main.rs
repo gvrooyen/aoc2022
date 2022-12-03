259bba460d7ec40fec40badcfb1d6adb5308f751
G-J van Rooyen <g-j@octoco.ltd>
2022-12-03 19:05:53 +0200

    Solved Day 3, Part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -35,6 +35,36 @@ fn priority(c: char) -> u32 {
     }
 }
 
+// Given a vector of strings, find the character common to each string.
+// If there is more than one such character, or no such character, return None.
+fn get_badge(rucksacks: &[&str]) -> Option<char> {
+    let mut items: Vec<HashSet<char>> = Vec::new();
+
+    // For each line in the input string, turn the line into a HashSet and add it to the `items`
+    // vector.
+    for rucksack in rucksacks {
+        let mut seen = HashSet::new();
+        for c in rucksack.chars() {
+            seen.insert(c);
+        }
+        items.push(seen);
+    }
+
+    // Calculate the intersection of all the HashSets in the `items` vector.
+    let mut intersection = items[0].clone();
+    for item in items.iter().skip(1) {
+        intersection = intersection.intersection(item).cloned().collect();
+    }
+
+    // If there is exactly one character in the intersection, return it.
+    // Otherwise, return None.
+    if intersection.len() == 1 {
+        Some(*intersection.iter().next().unwrap())
+    } else {
+        None
+    }
+}
+
 fn main() {
     // Read the input from `data/input.txt`, and calculate the priority of the duplicate item in
     // each line. Accumulate the sum of the results.
@@ -44,7 +74,20 @@ fn main() {
         .map(|line| find_duplicate(line).expect("No duplicate found"))
         .map(priority)
         .sum();
-    println!("{}", sum);
+    println!("Part 1: {}", sum);
+
+    // Read the input from `data/input.txt` 3 lines at a time, and pass it to the `get_badge`
+    // function to find the common item ("badge"). For each badge, calculate the priority and
+    // accumulate the sum of the results.
+    let sum: u32 = std::fs::read_to_string("data/input.txt")
+        .expect("Failed to read input file")
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|chunk| get_badge(chunk).expect("No badge found"))
+        .map(priority)
+        .sum();
+    println!("Part 2: {}", sum);
 }
 
 #[cfg(test)]
@@ -74,5 +117,24 @@ mod tests {
         assert_eq!(priority('z'), 26);
         assert_eq!(priority('A'), 27);
         assert_eq!(priority('Z'), 52);
+    }
+
+    #[test]
+    fn test_get_badges() {
+        // Collect the first 3 lines of the test data into a vector, and get those lines' badge.
+        let rucksacks: Vec<&str> = TEST_DATA
+            .iter()
+            .map(|(rucksack, _)| *rucksack)
+            .take(3)
+            .collect();
+        assert_eq!(get_badge(&rucksacks), Some('r'));
+        // Do the same for the next 3 lines.
+        let rucksacks: Vec<&str> = TEST_DATA
+            .iter()
+            .map(|(rucksack, _)| *rucksack)
+            .skip(3)
+            .take(3)
+            .collect();
+        assert_eq!(get_badge(&rucksacks), Some('Z'));
     }
 }
