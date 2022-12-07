39cb52649ccf4e33b2cc213378f333f413cb2f19
G-J van Rooyen <g-j@octoco.ltd>
2022-12-07 14:18:01 +0200

    The initial stack part of the input is now parsed correctly into vectors

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -5,8 +5,9 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-type Stack = Vec<VecDeque<char>>;
+type Stack = Vec<Vec<char>>;
 
+#[derive(Debug)]
 struct Move {
     qty: u32,
     src: usize,
@@ -19,36 +20,55 @@ fn parse_input(filename: &str) -> (Stack, Vec<Move>) {
     // This 2D vector of characters initially represents the first part of the input file, which
     // describes the initial state of the stacks.
     let mut raw_stack: Vec<Vec<char>> = Vec::new();
-    // This will eventually store the initial state of the stacks.
-    let mut stack = Vec::new();
     // This will eventually store the moves between stacks.
     let mut moves = Vec::new();
     let mut num_stacks = 0;
 
     let reader = BufReader::new(File::open(filename).expect("Could not open file"));
-    let lines = reader.lines().enumerate();
+    let lines = reader.lines();
 
-    // Read the first part of the input file, which describes the initial state of the stacks. This
-    // part is terminated by a blank line. There are $(w + 1)/4$ stacks, where $w$ is the width of
-    // each line in this part of the input file. In each line, the character at position $4i - 1$
-    // is the next-from-top contents of stack $i$, which is added to the `raw_stack` vector.
-    for (i, line) in lines {
+    let mut file_part = 0;
+
+    for (i, line) in lines.enumerate() {
         let line = line.expect("Could not read line");
         if i == 0 {
             num_stacks = (line.len() + 1) / 4;
         }
         if line.is_empty() {
-            break;
+            file_part += 1;
+            continue;
         }
-        raw_stack.push(Vec::new());
-        for (j, c) in line.chars().enumerate() {
-            if j % 4 == 1 {
-                raw_stack[i].push(c);
+
+        if file_part == 0 {
+            // This is the first part of the input file, which describes the initial state of the
+            // stacks.
+            raw_stack.push(Vec::new());
+            for (j, c) in line.chars().enumerate() {
+                if j % 4 == 1 {
+                    raw_stack[i].push(c);
+                }
             }
+        } else {
         }
     }
 
     println!("{:?}", raw_stack);
+
+    // This will eventually store the initial state of the stacks.
+    let mut stack: Stack = vec![Vec::new(); num_stacks];
+
+    // Convert the `raw_stack` vector into a `Stack` container. This is done by iterating over
+    // `raw_stack` from next-to-last to first, and pushing each non-space character to the
+    // corresponding `stack` vector.
+    for i in (0..num_stacks - 1).rev() {
+        for (j, c) in raw_stack[i].iter().enumerate() {
+            if *c != ' ' {
+                stack[j].push(*c);
+            }
+        }
+    }
+
+    println!("{:?}", stack);
 
     (stack, moves)
 }
