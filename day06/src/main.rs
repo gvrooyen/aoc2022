11005e30f1da09cb5074e5ea4996776659076138
G-J van Rooyen <g-j@octoco.ltd>
2022-12-08 22:30:20 +0200

    Solved both parts of Day 6 at once, with just a parameter change

---
+++ day06/src/main.rs
@@ -0,0 +1,58 @@
+// Advent of Code 2022 - Day 5
+// Find start-of packet markers in a stream of characters.
+
+use std::collections::{HashSet, VecDeque};
+use std::fs;
+
+fn find_marker(stream: Vec<char>, len: usize) -> Option<usize> {
+    let mut win = VecDeque::new();
+    win.extend(stream.iter().take(len));
+    let mut i = len;
+    while i < stream.len() {
+        let chars: HashSet<char> = HashSet::from_iter(win.iter().cloned());
+        if chars.len() == len {
+            return Some(i);
+        }
+        win.pop_front();
+        win.push_back(stream[i]);
+        i += 1;
+    }
+    None
+}
+
+fn main() {
+    // Assume that the input file has only one line, and read it.
+    let stream = fs::read_to_string("data/input.txt").expect("Unable to read file");
+    let stream: Vec<char> = stream.chars().collect();
+    let pos = find_marker(stream, 14);
+    println!("Found marker at position {}", pos.unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_marker() {
+        assert_eq!(
+            find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb".chars().collect(), 4),
+            Some(7)
+        );
+        assert_eq!(
+            find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz".chars().collect(), 4),
+            Some(5)
+        );
+        assert_eq!(
+            find_marker("nppdvjthqldpwncqszvftbrmjlhg".chars().collect(), 4),
+            Some(6)
+        );
+        assert_eq!(
+            find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".chars().collect(), 4),
+            Some(10)
+        );
+        assert_eq!(
+            find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".chars().collect(), 4),
+            Some(11)
+        );
+    }
+}
