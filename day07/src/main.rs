25f1578f238711ea34116eb2c01ec26040e94abc
G-J van Rooyen <g-j@octoco.ltd>
2022-12-13 00:44:52 +0200

    Refactored so that tests pass with full-path keys

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,6 +1,9 @@
 // Advent of Code 2022 - Day 7
 // kdirstat
 
+// TODO: The current implementation assumes unique node names. Refactor so that the main data
+// structure is a proper tree.
+
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -21,6 +24,8 @@ enum ModeType {
 struct Node {
     size: u32,
     node_type: NodeType,
+    // The name of the node itself, without the full path
+    name: String,
     parent: Option<String>,
     children: Vec<String>,
 }
@@ -31,13 +36,16 @@ impl Node {
             size: 0,
             node_type: NodeType::Dir,
             parent: None,
+            name: String::from("/"),
             children: Vec::new(),
         }
     }
 }
 
 struct Context {
+    // Nodes are indexed by their full path
     nodes: HashMap<String, Node>,
+    // The full path of the current node, e.g. /a/d/d.log
     pwd: String,
     mode: ModeType,
 }
@@ -57,13 +65,15 @@ impl Context {
     fn add_node(&mut self, node_type: NodeType, name: &str, size: u32) {
         let node = Node {
             size,
+            name: name.to_string(),
             node_type,
             parent: Some((*self.pwd).to_string()),
             children: Vec::new(),
         };
-        self.nodes.insert(String::from(name), node);
-        let parent_node = self.nodes.get_mut(&self.pwd).unwrap();
-        parent_node.children.push(String::from(name));
+        self.nodes.insert(format!("{}/{}", self.pwd, name), node);
+        let parent_node_key = Some(self.pwd.clone());
+        let parent_node = self.nodes.get_mut(&parent_node_key.unwrap()).unwrap();
+        parent_node.children.push(name.to_string());
     }
 
     fn parent(&mut self) -> Option<String> {
@@ -72,14 +82,25 @@ impl Context {
 
     // Caculate the total size of a node, by adding its own size to the size of its children and
     // theirs, recursively.
-    fn node_size(&self, name: &str) -> u32 {
-        let node = self.nodes.get(name).unwrap();
+    fn node_size(&self, pwd: &str) -> u32 {
+        println!("node_size: {}", pwd);
+        let node = self.nodes.get(pwd).unwrap();
         let mut size = node.size;
-        println!("{:?}", node.children);
         for child in &node.children {
-            size += self.node_size(child.as_str());
+            size += self.node_size(format!("{}/{}", pwd, child).as_str());
         }
         size
+    }
+
+    // Calculate the total size of all directories of at most the given size.
+    fn total_size(&self, size: u32) -> u32 {
+        let mut total = 0;
+        for (name, node) in &self.nodes {
+            if node.node_type == NodeType::Dir && self.node_size(name.as_str()) <= size {
+                total += self.node_size(name.as_str());
+            }
+        }
+        total
     }
 
     fn parse_line(&mut self, line: &str) {
@@ -90,7 +111,8 @@ impl Context {
                     // Change directory.
                     match w.next() {
                         Some("..") => self.pwd = self.parent().unwrap(),
-                        Some(name) => self.pwd = name.to_string(),
+                        Some("/") => self.pwd = String::from("/"),
+                        Some(name) => self.pwd = format!("{}/{}", self.pwd, name.to_string()),
                         None => panic!("cd: missing operand"),
                     }
                     self.mode = ModeType::Command;
@@ -117,7 +139,9 @@ impl Context {
                 let file = w.next().unwrap();
                 self.add_node(NodeType::File, file, size);
             }
-            None => {}
+            None => {
+                !panic!("Unknown file type");
+            }
         }
     }
 }
@@ -137,7 +161,7 @@ fn parse_input(filename: &str) -> Context {
 
 fn main() {
     let context = parse_input("data/input.txt");
-    println!("{:?}", context.pwd);
+    println!("{:?}", context.total_size(100000));
 }
 
 #[cfg(test)]
@@ -155,16 +179,16 @@ mod tests {
         assert_eq!(context.pwd, "/");
         context.parse_line("$ ls");
         context.parse_line("dir a");
-        assert_eq!(context.nodes["a"].size, 0);
+        assert_eq!(context.nodes["//a"].size, 0);
         context.parse_line("14848514 b.txt");
-        assert_eq!(context.nodes["b.txt"].size, 14848514);
+        assert_eq!(context.nodes["//b.txt"].size, 14848514);
         context.parse_line("8504156 c.dat");
-        assert_eq!(context.nodes["c.dat"].size, 8504156);
+        assert_eq!(context.nodes["//c.dat"].size, 8504156);
         context.parse_line("dir d");
-        assert_eq!(context.nodes["d"].size, 0);
+        assert_eq!(context.nodes["//d"].size, 0);
         context.parse_line("$ cd a");
-        assert_eq!(context.pwd, "a");
-        assert_eq!(context.nodes["a"].parent, Some("/".to_string()));
+        assert_eq!(context.pwd, "//a");
+        assert_eq!(context.nodes["//a"].parent, Some("/".to_string()));
         context.parse_line("$ cd ..");
         assert_eq!(context.pwd, "/");
     }
@@ -172,7 +196,15 @@ mod tests {
     #[test]
     fn test_parse_input() {
         let context = parse_input("data/test.txt");
-        println!("{:?}", context.nodes);
-        assert_eq!(context.node_size("e"), 584);
+        assert_eq!(context.node_size("//a/e"), 584);
+        assert_eq!(context.node_size("//a"), 94853);
+        assert_eq!(context.node_size("//d"), 24933642);
+        assert_eq!(context.node_size("/"), 48381165);
+    }
+
+    #[test]
+    fn test_total_size() {
+        let context = parse_input("data/test.txt");
+        assert_eq!(context.total_size(100000), 95437);
     }
 }
